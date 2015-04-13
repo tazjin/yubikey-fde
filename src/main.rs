64a99e0796e5bb6d255fa180aaee5ea9c99c304c
Vincent Ambo <vincent@kivra.com>
2015-04-14 01:42:31 +0200

    [main] Remove testing functions

--- src/main.rs
+++ src/main.rs
@@ -3,35 +3,14 @@
 
 extern crate inotify;
 extern crate libc;
-extern crate rand;
 extern crate regex;
 extern crate rustc_serialize;
 
-use rand::*;
 use std::io::ErrorKind;
 
 mod askpass;
 mod socket;
 mod yubikey;
-
-fn random_challenge() -> Vec<u8> {
-    let mut vec: Vec<u8> = Vec::new();
-    for _ in 0..64 {
-        vec.push(rand::random());
-    }
-    vec
-}
-
-fn yubikey_testing() -> Result<(), yubikey::YubikeyError> {
-    yubikey::yubikey_init();
-
-    let yk = try!(yubikey::get_yubikey());
-    let challenge = &random_challenge();
-    let result = try!(yubikey::challenge_response(yk, 2, challenge, false));
-
-    println!("{}", result);
-    Ok(())
-}
 
 fn main() {
     println!("Checking for existing systemd-asks");
